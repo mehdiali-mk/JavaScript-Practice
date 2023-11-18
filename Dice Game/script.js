60b7e45b93b018c7d91a647039ea3fa7232d0982
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2023-11-18 15:05:19 +0530

    Added total score and footer

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -1,7 +1,12 @@
 const rollDiceEl = document.querySelector(".roll-dice");
 const dice1El = document.querySelector(".dice-1");
 const dice2El = document.querySelector(".dice-2");
+const p1ScoreEl = document.querySelector(".p1-score");
+const p2ScoreEl = document.querySelector(".p2-score");
 const resultEl = document.querySelector(".result");
+
+let p1Score = 0,
+  p2Score = 0;
 
 rollDiceEl.addEventListener("click", function () {
   const rand1 = Math.trunc(Math.random() * 6) + 1;
@@ -12,8 +17,12 @@ rollDiceEl.addEventListener("click", function () {
 
   if (rand1 > rand2) {
     resultEl.textContent = "Player 1 is Winner ⛳";
+    p1Score++;
+    p1ScoreEl.textContent = p1Score;
   } else if (rand2 > rand1) {
     resultEl.textContent = "Player 2 is Winner ⛳";
+    p2Score++;
+    p2ScoreEl.textContent = p2Score;
   } else {
     resultEl.textContent = "Draw !";
   }
